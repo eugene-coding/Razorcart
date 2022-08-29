6196c9d07e3db402d35c3dc7ec43078dba993713
Eugene <eugene.code@yandex.ru>
2022-08-29 16:55:56 +0300

    Added the base service class

---
+++ Razorcart/Services/Service.cs
@@ -0,0 +1,13 @@
+﻿using Razorcart.Data;
+
+namespace Razorcart.Services;
+
+public abstract class Service
+{
+    public Service(Context context)
+    {
+        Context = context;
+    }
+
+    private protected Context Context { get; init; }
+}
