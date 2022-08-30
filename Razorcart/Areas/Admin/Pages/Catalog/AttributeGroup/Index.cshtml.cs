5614d21bb271810e4b7a094a39274648a9ac9079
Eugene <eugene.code@yandex.ru>
2022-08-30 15:35:37 +0300

    Added breadcrumbs for AttributeGroup Index page

--- Razorcart/Areas/Admin/Pages/Catalog/AttributeGroup/Index.cshtml.cs
+++ Razorcart/Areas/Admin/Pages/Catalog/AttributeGroup/Index.cshtml.cs
@@ -1,8 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Localization;
 
 using Razorcart.Areas.Admin.Services.Catalog;
+using Razorcart.Common;
 
 namespace Razorcart.Areas.Admin.Pages.Catalog.AttributeGroup;
 
@@ -10,19 +10,32 @@ public class IndexModel : PageModel
 {
     private readonly IAttributeGroupService _service;
 
-    public IndexModel(IStringLocalizer<IndexModel> text, IAttributeGroupService service)
+    public IndexModel(IStringLocalizer<IndexModel> text, LinkGenerator linkGenerator, IAttributeGroupService service)
     {
         _service = service;
 
         Text = text;
-        AttributeGroups = new List<Data.Models.AttributeGroup>();
+        LinkGenerator = linkGenerator;
+        AttributeGroups = default!;
+        Breadcrumbs = default!;
     }
 
     public IStringLocalizer<IndexModel> Text { get; init; }
+    public LinkGenerator LinkGenerator { get; init; }
+
+    public IEnumerable<Breadcrumb> Breadcrumbs { get; private set; }
     public IReadOnlyCollection<Data.Models.AttributeGroup> AttributeGroups { get; private set; }
 
     public async Task OnGetAsync()
     {
+        var query = Request.QueryString.ToString();
+
+        Breadcrumbs = new List<Breadcrumb>()
+        {
+            new ("Главная", LinkGenerator.GetPathByPage(HttpContext, "/Index")),
+            new (Text["HeadingTitle"], Request.Path + Request.QueryString)
+        };
+
         AttributeGroups = await _service.GetAttributeGroupsAsync();
     }
 }
---
+++ Razorcart/Common/Breadcrumb.cs
@@ -0,0 +1,21 @@
+﻿namespace Razorcart.Common;
+
+public struct Breadcrumb
+{
+    public Breadcrumb(string name, string? url)
+    {
+        url ??= string.Empty;
+
+        Name = name;
+        Url = new Uri(url, UriKind.RelativeOrAbsolute);
+    }
+
+    public Breadcrumb(string name, Uri url)
+    {
+        Name = name;
+        Url = url;
+    }
+
+    public string Name { get; set; }
+    public Uri Url { get; set; }
+}
