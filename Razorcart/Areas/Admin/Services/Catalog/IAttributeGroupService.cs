c91ca3e80dcbc726da8db13c9d163d3416bc06cf
eugene-coding <eugene.code@yandex.ru>
2022-09-05 23:04:56 +0300

    Added GetAttributeGroup overload with FilterData argument

--- Razorcart/Areas/Admin/Services/Catalog/IAttributeGroupService.cs
+++ Razorcart/Areas/Admin/Services/Catalog/IAttributeGroupService.cs
@@ -1,9 +1,11 @@
-﻿using Razorcart.Data.Models;
+﻿using Razorcart.Data;
+using Razorcart.Data.Models;
 
 namespace Razorcart.Areas.Admin.Services.Catalog;
 
 public interface IAttributeGroupService
 {
     Task<List<AttributeGroup>> GetAttributeGroupsAsync();
+    Task<List<AttributeGroup>> GetAttributeGroupsAsync(FilterData filterData);
     Task DeleteAttributeGroupAsync(int id);
 }
