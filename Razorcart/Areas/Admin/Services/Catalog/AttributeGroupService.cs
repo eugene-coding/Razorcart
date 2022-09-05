7d6ce6104a454feea042ed84fce1402d54386656
eugene-coding <eugene.code@yandex.ru>
2022-09-05 23:04:56 +0300

    Added GetAttributeGroup overload with FilterData argument

--- Razorcart/Areas/Admin/Services/Catalog/AttributeGroupService.cs
+++ Razorcart/Areas/Admin/Services/Catalog/AttributeGroupService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
+using Razorcart.Areas.Admin.Services.Settings;
 using Razorcart.Data;
 using Razorcart.Data.Models;
 using Razorcart.Services;
@@ -8,15 +10,35 @@ namespace Razorcart.Areas.Admin.Services.Catalog;
 
 public class AttributeGroupService : Service, IAttributeGroupService
 {
-    public AttributeGroupService(Context context) : base(context)
+    private readonly ISettingService _settingService;
+
+    public AttributeGroupService(Context context, ISettingService settingService) : base(context)
     {
+        _settingService = settingService;
     }
 
     public async Task<List<AttributeGroup>> GetAttributeGroupsAsync()
     {
-        var result = await Context.AttributeGroups
-            .Include(ag => ag.AttributeGroupDescriptions)
-            .ToListAsync();
+        var query = await GetAttributeGroupQueryAsync();
+
+        var result = await query.ToListAsync();
+
+        return result;
+    }
+
+    public async Task<List<AttributeGroup>> GetAttributeGroupsAsync(FilterData filterData)
+    {
+        var initialQuery = await GetAttributeGroupQueryAsync();
+
+        var orderedQuery = filterData.Order is Order.Ascending
+             ? initialQuery.OrderBy(a => a.SortOrder)
+             : initialQuery.OrderByDescending(a => a.SortOrder);
+
+        var limitedQuery = orderedQuery
+            .Skip(filterData.Skip)
+            .Take(filterData.Take);
+
+        var result = await limitedQuery.ToListAsync();
 
         return result;
     }
@@ -32,5 +54,16 @@ public class AttributeGroupService : Service, IAttributeGroupService
 
             await Context.SaveChangesAsync();
         }
+    }
+
+    private async Task<IIncludableQueryable<AttributeGroup, IEnumerable<AttributeGroupDescription>>> GetAttributeGroupQueryAsync()
+    {
+        var languageId = await _settingService.GetLanguageIdAsync();
+
+        var query = Context.AttributeGroups
+            .Include(ag => ag.AttributeGroupDescriptions
+                .Where(agd => agd.LanguageId == languageId));
+
+        return query;
     }
 }
