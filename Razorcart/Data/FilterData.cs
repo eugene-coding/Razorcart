a0e7b3c148e3300917b8e0da24f75b60d27a4e72
eugene-coding <eugene.code@yandex.ru>
2022-09-05 22:34:46 +0300

    Created FilterData stuct to filter and limit SQL queries

---
+++ Razorcart/Data/FilterData.cs
@@ -0,0 +1,34 @@
+﻿namespace Razorcart.Data;
+
+public struct FilterData
+{
+    public FilterData(int itemsPerPage)
+    {
+        Take = itemsPerPage < 0 ? 0 : itemsPerPage;
+    }
+
+    public FilterData(int itemsPerPage, int page) : this(itemsPerPage)
+    {
+        if (page < 1)
+        {
+            page = 1;
+        }
+
+        Skip = (page - 1) * itemsPerPage;
+    }
+
+    public FilterData(int itemsPerPage, int page, Order order) : this(itemsPerPage, page)
+    {
+        Order = order;
+    }
+
+    public Order Order { get; private set; } = Order.Ascending;
+    public int Skip { get; private set; } = 0;
+    public int Take { get; private set; }
+}
+
+public enum Order
+{
+    Ascending,
+    Descending
+}
