c21d7fb3296472493179a110213d5b17b54ee2a7
eugene-coding <eugene.code@yandex.ru>
2022-09-05 22:56:46 +0300

    Added Name field for Language

--- Razorcart/Data/Models/Language.cs
+++ Razorcart/Data/Models/Language.cs
@@ -1,8 +1,16 @@
-﻿namespace Razorcart.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
+using System.ComponentModel.DataAnnotations;
+
+namespace Razorcart.Data.Models;
+
+[Index(nameof(Name), IsUnique = true)]
 public class Language
 {
     public int Id { get; set; }
+
+    [MaxLength(32)]
+    public string Name { get; set; } = string.Empty;
 
     public virtual IList<AttributeGroupDescription> AttributeGroupDescriptions { get; set; } = default!;
 }
